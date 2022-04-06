e75a72e8dbdbb9714ee31636ea1894230e5dd336
obr-note <k33mdukb9n@yahoo.co.jp>
2022-04-06 18:39:57 +0900

    04-factory

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -46,4 +46,26 @@ RSpec.describe Project, type: :model do
 
     expect(other_project).to be_valid
   end
+
+  describe "late status" do
+    it "is late when the due data is past today" do
+      project = FactoryBot.create(:project, :due_yesterday)
+      expect(project).to be_late
+    end
+
+    it "is late when the due data is today" do
+      project = FactoryBot.create(:project, :due_today)
+      expect(project).to_not be_late
+    end
+
+    it "is late when the due data is in the future" do
+      project = FactoryBot.create(:project, :due_tomorrow)
+      expect(project).to_not be_late
+    end
+  end
+
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
+  end
 end
